79f2ac1dea80e343abb68f0bbae9c64c582d1539
RevanYor <70781398+RevanYor@users.noreply.github.com>
2023-07-14 20:57:21 -0400

    create the grid function

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 let range = document.querySelector('#rangeButton');
 let gridValue = document.querySelector('#gridValue');
 let container = document.querySelector('#container');
-let div = 0;
+let apply = document.querySelector('#apply');
+let div = document.querySelector('.gridElement');
+let clear = document.querySelector('#clear');
 
+//get the number of divs in the grid
 gridValue.textContent = range.value;
 range.addEventListener("input", (event) => {gridValue.textContent = event.target.value;});
-range.addEventListener("input", () => {
-    div = document.createElement('div');
-    container.appendChild(div);
+
+//Create the div grid with apply button
+apply.addEventListener("click", () => {
+    let count = parseInt(gridValue.textContent);
+    for(i=0;i<count;i++) {
+        grid();
+    }
+    console.log(count);
+})
+clear.addEventListener("click", () => {
+    let count = parseInt(gridValue.textContent);
+    for(i=0;i<count;i++){
+        div.remove();
+    }
 })
 
-
-
-
-/* function grids() {
-    for(i = 0; i <= gridValue.textContent; i++) {
-        div = document.createElement('div');
-        container.appendChild(div);
-    }
-
+function grid() {
+    div = document.createElement('div');
+    div.classList.add('gridElement');
+    container.appendChild(div);
 }
-grids(); */
